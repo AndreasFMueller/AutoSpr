2e9b4ecc0a8f793e27993b625f26e7e333c4ec23
Andreas Mueller <andreas.mueller@hsr.ch>
2013-01-10 14:36:20 +0100

    Aufgabensammlung hinzugefuegt

---
+++ aufgaben/buildchapter.sh
@@ -0,0 +1,17 @@
+#! /bin/sh
+#
+# build a chapter include file
+#
+# (c) 2012 Prof Dr Andreas Mueller, Hochschule Rapperswil
+#
+if [ ! -d "${1}" ]
+then
+	echo "must specify directory"
+	exit 1
+fi
+dir="${1}"
+cd ${dir}
+ls *.tex | awk '{
+	file = substr($1, 1, length($1) - 4)
+	printf("\\aufgabe{%s}{%s}\n", "'"${dir}"'", file)
+}'
