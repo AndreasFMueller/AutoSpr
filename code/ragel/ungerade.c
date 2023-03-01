6718798ea813a6380014fa2b01a9dccaaa81a8c8
Andreas Müller <andreas.mueller@ost.ch>
2023-03-01 22:13:53 +0100

    ungerade FSM added

---
+++ code/ragel/ungerade.c
@@ -0,0 +1,37 @@
+/*
+ * ungerade.c --- simple ragel example
+ *
+ * (c) 2023 Prof Dr Andreas Müller
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+%%{
+	machine ungerade;
+	action istungerade { res = 1; }
+}%%
+
+%% write data;
+
+int	main(int argc, char *argv[]) {
+	int	cs, res = 0;
+	int	i;
+	for (i = 1; i < argc; i++) {
+		res = 0;
+		char	*p = argv[i];
+		char	*pe = p + strlen(p);
+		char	*eof = pe;
+		%%{
+			ungerade :=  ('0'*'1'('0'|'1'('0')*'1')*) %istungerade;
+			write init;
+			write exec;
+		}%%
+		
+		printf("p = %p, pe = %p, eof = %p\n", p, pe, eof);
+		printf("cs = %d, res = %d\n", cs, res);
+		printf("%s %sgerade Anzahl Einsen\n", argv[i],
+			(res) ? "un" : "");
+	}
+	return EXIT_SUCCESS;
+}
